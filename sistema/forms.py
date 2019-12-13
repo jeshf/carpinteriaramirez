39895d761151bcbb07cfc30806dc8e6ea51a66cc
jesushf <engineer_jesushf@outlook.com>
2019-12-13 10:08:51 -0600

    enhacements on the image.html file

--- sistema/forms.py
+++ sistema/forms.py
@@ -4,7 +4,7 @@ class ContactForm(forms.Form):
     usuario = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder':'Escribe tu nombre'}))
     mensaje = forms.CharField(
         max_length=1000,
-        widget=forms.Textarea(attrs={'rows': 2, 'cols': 35, 'placeholder':'Escribe aqui'})
+        widget=forms.Textarea(attrs={'rows': 2, 'cols': 35, 'placeholder':'Escribe aqui tu comentario'})
         #widget=forms.TextInput(attrs={'size': 20})#,
         #help_text='Escribe aqui'
     )
