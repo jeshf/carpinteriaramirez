3de74adcf46dced5e0aff610cf614dcc5cab2261
jesushf <engineer_jesushf@outlook.com>
2020-02-03 11:47:40 -0600

    sign up form and logout view working

--- sistema/forms.py
+++ sistema/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import CustomUser
 
 class ContactForm(forms.Form):
-    usuario = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder':'Escribe tu nombre'}))
+    usuario = forms.CharField(max_length=45, widget=forms.TextInput(attrs={'placeholder':'Escribe tu nombre'}))
     mensaje = forms.CharField(
         max_length=1000,widget=forms.Textarea(attrs={'rows': 2, 'cols': 30, 'placeholder':'Escribe aqui tu comentario'}))
     flag = forms.CharField(widget=forms.HiddenInput())
@@ -27,14 +27,48 @@ class ResponseForm(forms.Form):
             raise forms.ValidationError('El campo respuesta no puede estar vacío')
 
 class SignInForm(forms.Form):
-    usuario = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Escribe tu nombre de usuario',
+    usuario = forms.CharField(max_length=45, widget=forms.TextInput(attrs={'placeholder': 'Escribe tu nombre de usuario',
                                                                            'class':'input-lg form-control'}))
     password = forms.CharField(
-        max_length=1000,
+        max_length=45,
         widget=forms.PasswordInput(attrs={'placeholder': 'Escribe aqui tu contraseña','class':'input-lg form-control'}))
+    flag = forms.CharField(widget=forms.HiddenInput())
     def clean(self):
         cleaned_data = super(SignInForm, self).clean()
         usuario = cleaned_data.get('usuario')
         password = cleaned_data.get('password')
         if not usuario or not password:
             raise forms.ValidationError('Los campos usuario y/o password no pueden estar vacios')
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=45, widget=forms.TextInput(attrs={'placeholder': 'Escribe tu nombre de usuario',
+                                                                           'class':'input-lg form-control'}))
+    password = forms.CharField(
+        max_length=45,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Escribe aqui tu contraseña','class':'input-lg form-control'}))
+    password2 = forms.CharField(
+        max_length=45,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Escribe otra vez tu contraseña','class':'input-lg form-control'}))
+    email = forms.CharField(max_length=60, widget=forms.EmailInput(attrs={'placeholder': 'Escribe tu correo electrónico',
+                                                                           'class':'input-lg form-control'}))
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Escribe tu nombre(s)',
+                                                                           'class':'input-lg form-control'}))
+    firstLastName = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Escribe tu apellido paterno',
+                                                                         'class': 'input-lg form-control'}))
+
+    secondLastName = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Escribe tu apellido materno',
+                                                                         'class': 'input-lg form-control'}))
+    flag = forms.CharField(widget=forms.HiddenInput())
+    def clean(self):
+        cleaned_data = super(SignUpForm, self).clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+        email = cleaned_data.get('email')
+        name = cleaned_data.get('name')
+        firstLastName = cleaned_data.get('firstLastName')
+        secondLastName = cleaned_data.get('secondLastName')
+        if not username or not password or not password2 or not email or not name or not firstLastName or not secondLastName :
+            raise forms.ValidationError('Por favor llene todos los campos')
+        if password != password2:
+            raise forms.ValidationError('Las contraseñas no pueden ser diferentes')
--- sistema/views.py
+++ sistema/views.py
@@ -1,14 +1,14 @@
 #from rest_framework.parsers import JSONParser
 from django.contrib.auth import get_user_model
 from .forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template.loader import get_template
 from sistema.serializers import *
 from rest_framework import generics, permissions
 from rest_framework import viewsets
 #from rest_auth.registration.views import RegisterView
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.views.generic.detail import DetailView
 from .forms import SignInForm
 from rest_framework.views import APIView
@@ -67,25 +67,33 @@ def image(request,pk):
 
 class Login(APIView):
     def post(self, request, format=None):
-        #data = json.loads(request.body)
         data = request.data
-        username = data.get('usuario')
-        password = data.get('password')
-        account = authenticate(username=username, password=password)
-        if account is not None:
-            user = get_user_model().objects.get(username=username)
-            if user.is_active:
-                login(request, account)
-                serialized = UserSerializer(account)
-                return render(request, 'profile.html', {'object': serialized.data})
+        flag = data.get('flag')
+        if flag == "login":
+            username = data.get('usuario')
+            password = data.get('password')
+            account = authenticate(username=username, password=password)
+            if account is not None:
+                user = get_user_model().objects.get(username=username)
+                if user.is_active:
+                    login(request, account)
+                    serialized = UserSerializer(account)
+                    return render(request, 'profile.html', {'object': serialized.data})
+                else:
+                    return HttpResponse('Unauthorized', status=401)
             else:
                 return HttpResponse('Unauthorized', status=401)
+        elif flag=="register":
+            serializer = UserSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+            return render(request, 'profile.html', {'object': serializer.data})
         else:
-            return HttpResponse('Unauthorized', status=401)
+            return HttpResponse('Bad Request', status=400)
     def get(self, request, format=None):
-        #data = json.loads(request.body)
+        formu = SignUpForm()
         form = SignInForm()
-        return render(request, 'login.html', {'form': form})
+        return render(request, 'login.html', {'form': form,'formu':formu})
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser)
@@ -106,3 +114,8 @@ class PaymentViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser)
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        logout(request)
+        return HttpResponseRedirect("/api/login/")
